fdfbf1f133996593d1773db5ddaaef99839511f7
Jeroen.vandenHoven <jeroen.vandenhoven@eneco.com>
2024-07-01 08:20:32 +0200

    Drop python 3.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_namespace_packages, setup
 setup(
     name="pytalog",
     packages=find_namespace_packages(include=["pytalog.*"]),
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     version="0.0.1",
     license="MIT",
     author="Jeroen van den Hoven",
